ab80b859c2215badeb16cedc3a6d5a31ac3da8bb
ScramblesDD <143749619+ScramblesDD@users.noreply.github.com>
2025-10-30 16:45:20 -0500

    Refactor media query breakpoints and canvas props

--- src/sections/about.jsx
+++ src/sections/about.jsx
@@ -31,25 +31,21 @@ const About = () => {
         })
     */}
 
-    const isSmall = useMediaQuery({minWidth: 375})
-    const isMobile = useMediaQuery({minWidth: 768, maxWidth: 896})
-    const isTablet = useMediaQuery({minWidth: 896, maxWidth: 1024})
+    const isSmall = useMediaQuery({minWidth: 375, maxWidth: 767})
+    const isMobile = useMediaQuery({minWidth: 768, maxWidth: 895})
+    const isTablet = useMediaQuery({minWidth: 897, maxWidth: 1024})
 
     const sizes = calculateSizes(isSmall, isMobile, isTablet)
 
     return (
         <section className={"section-bg"}>
             <div className={"aboutBg"}>
-                <Canvas gl={{ localClippingEnabled: true }} shadows={true}>
-                    <PerspectiveCamera
-                        makeDefault
-                        fov={60}
-                        position={sizes.camPos}
-                        rotation={[83.6, 0, 0]}
-                    />
+                <Canvas gl={{ localClippingEnabled: true }} camera={{fov: 60, position: [0, -10.5, 0],
+                    rotation: [83.6, 0, 0] }} shadows>
                     <Environment background={false} preset={"apartment"} intensity={0.75}/>
                     <ambientLight />
                     <Display position={sizes.zPos} rotation={[90, 0, 0]} scale={sizes.displayScale}/>
+
                 </Canvas>
             </div>
         </section>
